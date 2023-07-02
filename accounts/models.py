0dda8b5e46f1897215c23a3de97c37afac380f83
Arvin Visi <arvinvisi03@gmail.com>
2023-07-02 13:45:06 +0330

    add CustomUser in models file

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=100, unique=True, verbose_name=_("Email"))
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
