9efd07bf49823d4cd91419afab2b928f1a2d910b
Arvin Visi <arvinvisi03@gmail.com>
2023-07-10 16:22:16 +0330

    add Product model in product app

--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=30, verbose_name='Title')
+    description = models.TextField(verbose_name='Description')
+    active = models.BooleanField(verbose_name="I's Active")
+
+    datetime_created = models.DateTimeField(default=timezone.now(), verbose_name='Date Time Created')
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name='Date Time Updated')
+
+    def __str__(self):
+        return self.title
