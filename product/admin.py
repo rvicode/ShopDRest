2c8727388f16f6c92d86ad50d13aac6b44ec42b7
Arvin Visi <arvinvisi03@gmail.com>
2023-07-12 16:31:44 +0330

    add CategoryAdmin

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Category
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'datetime_created', 'datetime_modified', 'active')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'datetime_created', 'datetime_modified', 'active')
