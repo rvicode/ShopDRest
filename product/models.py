dfb92d8c809696ca540bf6485c5b3155c53fdbd8
Arvin Visi <arvinvisi03@gmail.com>
2023-07-12 16:30:07 +0330

    update Category model

--- product/models.py
+++ product/models.py
@@ -5,6 +5,10 @@ from django.utils import timezone
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
+    active = models.BooleanField(verbose_name="I's Active")
+
+    datetime_created = models.DateTimeField(default=timezone.now(), verbose_name='Date Time Created')
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name='Date Time Updated')
 
     def __str__(self):
         return self.title
