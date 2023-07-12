4ceb2a7cb93ab9f7b12a66900f9b63866b30fc15
Arvin Visi <arvinvisi03@gmail.com>
2023-07-12 17:02:14 +0330

    add CategoryListSerializer

--- product/serializers.py
+++ product/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product, Category
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -14,4 +14,11 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('category', 'title', 'description', 'active', 'datetime_created')
+        read_only = True
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('title', )
         read_only = True
