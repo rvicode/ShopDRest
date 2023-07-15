10c3dae6bd8761e422e92167f251a98b4760fced
Arvin Visi <arvinvisi03@gmail.com>
2023-07-15 14:39:30 +0330

    add TabularInline

--- product/admin.py
+++ product/admin.py
@@ -3,9 +3,18 @@ from django.contrib import admin
 from .models import Product, Category, Comment
 
 
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ['user', 'message', 'active', 'parent', 'datetime_create']
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'datetime_created', 'datetime_modified', 'active')
+    inlines = [
+        CommentsInline,
+    ]
 
 
 @admin.register(Category)
