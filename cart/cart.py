f5db63933fde8435db757c21f1d58bc8b95c4195
Arvin Visi <arvinvisi03@gmail.com>
2023-07-18 10:52:55 +0330

    create iter, len, clear and get_total_price in class Cart

--- cart/cart.py
+++ cart/cart.py
@@ -53,3 +53,26 @@ class Cart:
         Mark session as modified to save changes
         """
         self.session.modified = True
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product_obj'] = product
+
+        for item in cart.values():
+            item['total_price'] = item['product_obj'].price * item['quantity']
+            yield item
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        del self.session['cart']
+        self.save()
+
+    def get_total_price(self):
+        return sum(item['product_obj'].price * item['quantity'] for item in self.cart.values())
