a59929c6d50efb9a6f6a97d587f544d354484156
Arvin Visi <arvinvisi03@gmail.com>
2023-07-18 11:24:05 +0330

    create MEDIA_URL AND MEDIA_ROOT and select in urls in config file

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -29,4 +31,4 @@ urlpatterns = [
 
     path('accounts/', include('accounts.urls')),
     path('product/', include('product.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
