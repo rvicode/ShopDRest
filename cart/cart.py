8787639f3276b6e9b86635e89aba1e0f561e0bfd
Arvin Visi <arvinvisi03@gmail.com>
2023-07-18 10:48:49 +0330

    add cart.py and create class Cart

---
+++ cart/cart.py
@@ -0,0 +1,20 @@
+from django.contrib import messages
+
+from product.models import Product
+
+
+class Cart:
+    def __init__(self, request):
+        """
+        Initialize the cart
+        """
+        self.request = request
+
+        self.session = request.session
+
+        cart = self.session.get('cart')
+
+        if not cart:
+            cart = self.session['cart'] = {}
+
+        self.cart = cart
