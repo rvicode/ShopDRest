08b22223a50c1f675ed81f23ed888d393ed8e877
Arvin Visi <arvinvisi03@gmail.com>
2023-07-20 11:34:19 +0330

    edit CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,22 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
 
-
 class CustomUser(AbstractUser):
-
     email = models.EmailField(max_length=100, unique=True, verbose_name=_("Email"))
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.email
 
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
-
-    def __str__(self):
-        username = self.username
-        if username:
-            return username
-        else:
-            return self.email
