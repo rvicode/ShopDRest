11cbe7c8266176eeaf34d3b330c4021c424b9560
Arvin Visi <arvinvisi03@gmail.com>
2023-07-24 11:42:13 +0330

    create OrderItem in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 
-from .models import Order
+from jalali_date.admin import TabularInlineJalaliMixin
+
+
+
+from .models import Order, OrderItem
+
+
+class CommentsInline(TabularInlineJalaliMixin, admin.TabularInline):
+    model = OrderItem
+    fields = ['order', 'product', 'quantity', 'price']
+    extra = 1
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['username', 'address', 'is_paid', 'datetime_create']
+    inlines = [
+        CommentsInline,
+    ]
+
+@admin.register(OrderItem)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price', 'datetime_create']
