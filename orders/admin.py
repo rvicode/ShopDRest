125de3e6fe8905979ae84a04d687a29e72d54787
Arvin Visi <arvinvisi03@gmail.com>
2023-07-24 11:30:10 +0330

    add Order model in admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['username', 'address', 'is_paid', 'datetime_create']
